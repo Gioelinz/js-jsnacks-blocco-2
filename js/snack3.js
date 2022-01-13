8b899624cdf4eb8941b76eacb356c34396739f5d
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-13 13:42:58 +0100

    add snack3 for method

--- js/snack3.js
+++ js/snack3.js
@@ -4,7 +4,7 @@ const names = ["Gioele", "Matteo", "Alessandro"];
 const surnames = ["Runza", "Imbimbo", "Basilico"];
 
 
-
+//! metodo while
 while (fakeInvites.length < 3) {
     const rndNum1 = Math.floor(Math.random() * names.length);
     const rndNum2 = Math.floor(Math.random() * surnames.length);
@@ -21,23 +21,23 @@ while (fakeInvites.length < 3) {
     }
 }
 
-console.table(fakeInvites)
 
-
-
-
-
-
-
-
+// ! metodo for
 /*
-do {
-    names.push(surnames[rndNum])
-} while (names == surnames)
+for (let i = 0; fakeInvites.length < 3; i++) {
+    const rndNum1 = Math.floor(Math.random() * names.length);
+    const rndNum2 = Math.floor(Math.random() * surnames.length);
 
-for (let i = 0; i < 3; i++) {
-    names.push(surnames[rndNum])
-    surnames.push(names[rndNum])
+    const name = names[rndNum1];
+    const lastname = surnames[rndNum2];
+
+    const fakePerson = `${name} ${lastname}`;
+
+    console.log('fake person:', fakePerson);
+
+    if (!fakeInvites.includes(fakePerson)) {
+        fakeInvites.push(fakePerson)
+    }
 }
-console.log(names, surnames)
 */
+console.table(fakeInvites)
